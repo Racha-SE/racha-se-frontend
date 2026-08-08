c092b7fda64394bcf3a4a4dca83df66d24987c8c
Kannawich Munsak <kannawich@gmail.com>
2026-08-09 00:23:19 +0700

    "chore: set up tailwind and shadcn ui"

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(import.meta.dirname, './src'),
+    },
+  },
   server: {
-        proxy: {
-            'v1': {
-                target: 'https://localhost:3000',
-                changeOrigin: true,
-            },
-        },
-    },          
+    proxy: {
+      v1: {
+        target: 'https://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
