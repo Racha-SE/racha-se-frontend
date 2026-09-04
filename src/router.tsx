e45b6922bf259e2f4bfa5fd07ca5e9fe77e7b8ce
Kantapon <dol2549l2006@gmail.com>
2026-09-04 15:57:47 +0700

    feat: add sign-in route

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { Navigate, createBrowserRouter } from "react-router-dom";
 import { BranchPage } from "./pages/branch/branch-page";
 import { CashierPage } from "./pages/cashier/cashier-page";
 import { HqPage } from "./pages/hq/hq-page";
+import { SignInPage } from "./pages/sign-in/sign-in-page";
 
 export const router = createBrowserRouter([
   {
@@ -19,5 +20,9 @@ export const router = createBrowserRouter([
   {
     path: "/cashier",
     element: <CashierPage />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignInPage />,
   },
 ]);
